7238e56c69c29af5d3f3de1669a5785319c7602c
NiChrosia <nichrosian@gmail.com>
2023-12-03 21:26:21 -0600

    feat: stroke order extractor and anki deck creator

---
+++ kana_deck.py
@@ -0,0 +1,45 @@
+from genanki import Note, Deck, Package, BASIC_AND_REVERSED_CARD_MODEL
+from argparse import ArgumentParser
+from random import randrange
+from os import remove
+
+from save_kana import save_kana
+
+def make_kana_deck(katakana: bool, filename: str):
+    deck = Deck(randrange(1 << 30, 1 << 31), ("Kata" if katakana else "Hira") + "kana")
+    files = []
+
+    for consonant in list(" kstnhmyrw"):
+        for vowel in list("aiueo"):
+            if consonant + vowel in ["yi", "ye", "wu"]:
+                continue
+            
+            filename_c = consonant if consonant != " " else "_"
+            kana_filename = filename + filename_c + vowel + ".png"
+
+            save_kana(consonant, vowel, katakana, kana_filename)
+            files.append(kana_filename)
+
+            note = Note(model=BASIC_AND_REVERSED_CARD_MODEL, fields=[consonant + vowel, f"<img src={kana_filename}>"])
+            deck.add_note(note)
+
+    package = Package(deck)
+    package.media_files = files
+
+    package.write_to_file(filename)
+
+    for file in files:
+        remove(file)
+
+def main():
+    parser = ArgumentParser(prog="kana deck creator", description="creates an anki deck of either hiragana or katakana")
+    parser.add_argument("filename", help="the path to store the deck at")
+    parser.add_argument("-k", "--katakana", action="store_true", help="makes the deck only include katakana, or hiragana if absent")
+
+    args = parser.parse_args()
+    
+    make_kana_deck(args.katakana, args.filename)
+    print(f"saved to {args.filename}!")
+
+if __name__ == "__main__":
+    main()
---
+++ save_kana.py
@@ -0,0 +1,69 @@
+from PIL import Image
+from argparse import ArgumentParser
+
+START_X = 131
+START_Y = 133
+
+TILE_WIDTH = 161
+TILE_HEIGHT = 154
+
+KATA_OFFSET = TILE_WIDTH + 1
+
+HORIZ_OFFSET = 340
+VERT_OFFSET = 234
+
+VOWELS = list("aiueo")
+CONSONANTS = list(" kstnhmyrwn")
+
+all_kana: Image.Image | None = None
+
+def find_kana(c: str, v: str, katakana: bool) -> tuple[int, int]:
+    c_index = CONSONANTS.index(c)
+    v_index = VOWELS.index(v)
+
+    x = START_X + HORIZ_OFFSET * v_index
+    y = START_Y + VERT_OFFSET * c_index
+
+    if katakana:
+        x += KATA_OFFSET
+
+    return (x, y)
+
+def save_kana(c: str, v: str, katakana: bool, filename: str):
+    """value in the format cvk, where v is a japanese vowel, c is a 
+    japanese consonant, and k is h or k, meaning hira- or kata-kana"""
+
+    global all_kana
+
+    if not all_kana:
+        all_kana = Image.open("kana.png")
+
+    x, y = find_kana(c, v, katakana)
+
+    kana = all_kana.copy().crop((x, y, x + TILE_WIDTH - 1, y + TILE_HEIGHT - 1))
+    kana.save(filename)
+
+def main():
+    parser = ArgumentParser(prog="kana saver", description="saves the stroke order for a specified kana")
+    parser.add_argument("consonant", help="the lowercase character of the consonant, or _ for no consonant")
+    parser.add_argument("vowel", help="the vowel of the kana (aiueo)")
+    parser.add_argument("-k", "--katakana", action="store_true", help="whether to use katakana, or hiragana if absent")
+
+    args = parser.parse_args()
+
+    c = args.consonant
+    v = args.vowel
+    k = args.katakana
+
+    find_kana_c = c if c != "_" else " "
+
+    filename_c = c if c != "_" else ""
+    filename_k = "_kata" if k else "_hira"
+
+    filename = filename_c + v + filename_k + ".png"
+
+    save_kana(find_kana_c, v, k, filename)
+    print(f"saved to {filename}!")
+
+if __name__ == "__main__":
+    main()
